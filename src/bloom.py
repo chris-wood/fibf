a97c7270f81ee915fd1da717b95755021b7d0586
Christopher Wood <christopherwood07@gmail.com>
2015-06-11 11:20:08 -0700

    started bit array

--- src/bloom.py
+++ src/bloom.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
 
 import sys
+import math
+import mmh3
+
+class BitArray(object):
+	def __init__(self, length):
+		self.length = int(math.ceil(length / 8))
+		self.array = (0,) * self.length
+
+	def __str__(self):
+		return "Array["+ str(self.length * 8) + "]: " + str(self.array)
+
+	def get(self, index):
+		index = index / 8
+		return self.array[index]
+
+	def set(self, index):
+		pass
 
 class BloomFilter(object):
 	def __init__(self):
 		pass
 
 def main(args):
+	n = int(sys.argv[1])
+	array = BitArray(n)
+	print array
 	pass
 
 if __name__ == "__main__":
