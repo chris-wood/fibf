396d75c2923ba90f3bb85968688a1473a5c13f2d
Christopher Wood <christopherwood07@gmail.com>
2015-06-11 13:58:06 -0700

    BF addition

--- src/bloom.py
+++ src/bloom.py
@@ -3,30 +3,70 @@
 import sys
 import math
 import mmh3
+import random
 
 class BitArray(object):
 	def __init__(self, length):
-		self.length = int(math.ceil(length / 8))
-		self.array = (0,) * self.length
+		self.indexSize = 32
+		self.length = int(math.ceil(length / self.indexSize))
+		self.array = [0,] * self.length
 
 	def __str__(self):
-		return "Array["+ str(self.length * 8) + "]: " + str(self.array)
+		return "Array["+ str(self.length * self.indexSize) + "]: " + str(self.array)
 
-	def get(self, index):
-		index = index / 8
-		return self.array[index]
+	def getBit(self, index):
+		offset = index % self.indexSize
+		index = index / self.indexSize
+		val = 1 if int((self.array[index] & (1 << offset))) > 0 else  0
+		return val
 
-	def set(self, index):
-		pass
+	def setBit(self, index):
+		offset = index % self.indexSize
+		index = index / self.indexSize
+		self.array[index] = self.array[index] | (1 << offset)
 
 class BloomFilter(object):
-	def __init__(self):
+	def __init__(self, n, k, seedLimit = 1000):
+		self.n = n
+		self.k = k
+
+		self.array = BitArray(n)
+
+		# Create the k hash functions
+		self.ivs = []
+		for i in range(k):
+			val = random.randint(0, seedLimit)
+			while val in self.ivs:
+				val = random.randint(0, seedLimit)
+			self.ivs.append(val)
+
+	def _hash(self, k, val):
+		return mmh3.hash(val, self.ivs[k])
+		
+	def insert(self, val):
+		for k in range(self.k):
+			digest = self._hash(k, val)
+			# insert each bit into the bit array
 		pass
+
+	def contains(self, element):
+		pass
+
+def playWithArray(n):
+	array = BitArray(n)
+	print array
+	array.setBit(4)
+	print array
+	print array.getBit(4)
+	print array.getBit(5)
 
 def main(args):
 	n = int(sys.argv[1])
-	array = BitArray(n)
-	print array
+	playWithArray(n)
+
+	
+	
+	# return OK
 	pass
 
 if __name__ == "__main__":
