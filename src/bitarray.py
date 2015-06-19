2c1a4060fa1972687e70ea9934cbb10f9130bd89
Christopher Wood <christopherwood07@gmail.com>
2015-06-19 08:28:35 -0700

    Style

--- src/bitarray.py
+++ src/bitarray.py
@@ -32,8 +32,8 @@ class CoutingArray(object):
 	''' Simple implementation of a bitarray or bitmap.
 	'''
 	def __init__(self, length):
-        ''' Each entry is a counter, not a single bit.
-        '''
+		''' Each entry is a counter, not a single bit.
+		'''
 		self.array = [0,] * self.length
 
 	def __str__(self):
@@ -43,16 +43,16 @@ class CoutingArray(object):
 		return self.length / base
 
 	def getValue(self, index):
-        return self.array[index]
+		return self.array[index]
 
 	def isEmpty(self, bit):
 		return (self.getCount(bit) == 0)
 
 	def addAt(self, index):
-        self.array[index] += 1
-
-    def removeAt(self, index):
+		self.array[index] += 1
+		
+	def removeAt(self, index):
 		if self.array[index] > 0:
-        	self.array[index] -= 1
+			self.array[index] -= 1
 		else:
 			pass # this not an exception
