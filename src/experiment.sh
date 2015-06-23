ac2faeb5758a5b4e88e0282057d1d8985dbbada2
Christopher Wood <christopherwood07@gmail.com>
2015-06-23 10:20:40 -0700

    added hashes and size as parameter

--- src/experiment.sh
+++ src/experiment.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+TIME=60
 arrivalRates=( 100 1000 10000 100000 )
 deletionRates=( 10 20 30 40 50 )
-TIME=60
-BFSIZE=256
-BFHASHES=3
+bfsizes=( 128 192 256 )
+bfhashes=( 3 4 5 6 7 8 )
 RANDOMSIZE=100
 
 for ar in "${arrivalRates[@]}"
 do
     for dr in "${deletionRates[@]}"
     do
-        python decayer.py $TIME $BFSIZE $BFHASHES 1 $ar $dr $RANDOMSIZE > out_$TIME_$BFSIZE_$BFHASHES_1_$ar_$dr_$RANDOMSIZE.out
+        for bfsize in "${bfsizes[@]}"
+        do
+            for bfhash in "${bfhashes[@]}"
+            do
+                python decayer.py $TIME $bfsize $bfhash 1 $ar $dr $RANDOMSIZE > out_$TIME_$bfsize_$bfhash_1_$ar_$dr_$RANDOMSIZE.out
+            done    
+        done
     done
 done
 
