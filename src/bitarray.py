497c842ca825358bf33629eda434f1355a044fe5
Christopher Wood <christopherwood07@gmail.com>
2015-07-08 08:53:04 -0700

    modulo array

--- src/bitarray.py
+++ src/bitarray.py
@@ -3,11 +3,12 @@ from threading import Thread, Lock
 
 class BitArray(object):
     ''' Simple implementation of a bitarray or bitmap.
+
+        It is not thread-safe.
     '''
     def __init__(self, length):
         self.indexSize = 32
         self.length = int(math.ceil(length / self.indexSize))
-        self.array = [0,] * self.length
 
     def __str__(self):
         return "Array["+ str(self.length * self.indexSize) + "]: " + str(self.array)
@@ -32,8 +33,40 @@ class BitArray(object):
         index = index / self.indexSize
         self.array[index] = self.array[index] | (1 << offset)
 
+class ModuloArray(object):
+    ''' Implementation of a bitmap where each entry has O(nlogn) bits
+        that roll over when they overflow.
+
+        It is not thread safe.
+    '''
+    def __init__(self, length):
+        ''' Each entry is a rolling counter, not a single bit.
+        '''
+        self.length = length
+        self.array = [1,] * self.length # everything is initialized to 1 to start
+
+    def __str__(self):
+        return "ModuloArray["+ str(self.length) + "]: " + str(self.array)
+
+    def size(self):
+        return self.length
+
+    def expansionFactor(self, base):
+        return self.length / base
+
+    def getValue(self, index):
+        return self.array[index]
+
+    def isEmpty(self, bit):
+        return (self.getValue(bit) == 1)
+
+    def addAt(self, index, val):
+        self.array[index] = val
+
 class CountingArray(object):
-    ''' Simple implementation of a bitarray or bitmap.
+    ''' Simple implementation of a counting bitmap.
+
+        It is thread safe.
     '''
     def __init__(self, length):
         ''' Each entry is a counter, not a single bit.
