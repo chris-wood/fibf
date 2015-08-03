f9afb0c8da41edcddff39569e5a8c69f15c87108
Christopher Wood <christopherwood07@gmail.com>
2015-08-03 12:59:14 -0700

    fixed the experiment

--- src/experiment.sh
+++ src/experiment.sh
@@ -5,9 +5,11 @@ TIME=3600
 # which are assumed to be core routers
 arrivalRates=( 28.45 142.25 711.25 3556.25 17781.25 88906.25 444531.25 )
 deletionRates=( 10 20 30 40 50 )
-bfsizes=( 128 192 256 )
-bfhashes=( 3 4 5 6 7 8 )
+bfsizes=( 1024 2048 4096 )
 RANDOMSIZE=100
+decayRate=1000
+
+
 
 for ar in "${arrivalRates[@]}"
 do
@@ -15,11 +17,8 @@ do
     do
         for bfsize in "${bfsizes[@]}"
         do
-            for bfhash in "${bfhashes[@]}"
-            do
-                echo python decayer.py $TIME $bfsize $bfhash 1 $ar $dr $RANDOMSIZE
-                python decayer.py $TIME $bfsize $bfhash 1 $ar $dr $RANDOMSIZE > out_$TIME_$bfsize_$bfhash_1_$ar_$dr_$RANDOMSIZE.out
-            done    
+            echo python decayer.py $TIME $bfsize $decayRate $ar $dr $RANDOMSIZE
+            python decayer.py $TIME $bfsize $decayRate $ar $dr $RANDOMSIZE > out_$TIME_$bfsize_$decayRate_$ar_$dr_$RANDOMSIZE.out
         done
     done
 done
